425531f4624057f780a7d8d509a8a1a0a2f06c9e
Sujit Pawar <sujitspawar9696@gmail.com>
2026-02-07 22:34:15 +0530

    Update SmsSimulator.java

--- SmsSimulator.java
+++ SmsSimulator.java
@@ -1,8 +1,9 @@
 public class SmsSimulator {
     public static void sendSMS(String patientName, int token) {
-        System.out.println("\n📩 SMS SENT TO PATIENT: ");
+        System.out.println("\nSMS SENT TO PATIENT: ");
         System.out.println("Hello " + patientName + " , your token " + token +
                            " will be called shortly.Please be near OPD.\n");
     }
 }
+
 
